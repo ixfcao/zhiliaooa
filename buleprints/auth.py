46c0d414a67a1241a960610265eeff50cfc896e9
ixfcao <2717176337@qq.com>
2024-11-18 15:16:38 +0800

    feat:验证码发送

--- buleprints/auth.py
+++ buleprints/auth.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+import string
+import random
+from flask import Blueprint, render_template, request
+from mako.runtime import capture
+
+
 from exts import mail
 from flask_mail import Message
-
+from flask import redirect
 bp = Blueprint("auth", __name__,url_prefix="/auth")
 
 @bp.route("/login")
@@ -12,6 +17,32 @@ def login():
 @bp.route("/register")
 def register():
     return render_template("register.html")
+
+# 验证码
+@bp.route("/captcha/email")
+def get_email_captcha():
+    # /captcha/email/<email>
+    # /captcha/email?email=xxx@qq.com
+    email = request.args.get("email")
+    # 4/6:数组、字母、数组和字母的组合
+    source = string.digits * 4
+    # 采样
+    captcha =  random.sample(source, 4)
+    captcha = " ".join(captcha)
+    message = Message(subject="知了传课注册验证码", recipients=["2717176337@qq.com"], body=f"测试！您的验证码是：{captcha}")
+    mail.send(message)
+
+    #  验证码可以存放在缓存中 memcached/redis
+    #  用数据库存储
+
+
+    return "success"
+
+
+
+
+
+
 
 @bp.route("/mail/test")
 def mail_test():

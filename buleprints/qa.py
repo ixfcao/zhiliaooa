49dfdec26219c27776b09eb09c30c8e1b34db6b1
ixfcao <2717176337@qq.com>
2024-11-18 21:26:15 +0800

    feat:发布问答功能实现

--- buleprints/qa.py
+++ buleprints/qa.py
@@ -1,9 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, redirect, url_for,g
+
+from buleprints.forms import QuestionForm
+from exts import db
+from models import QuestionModel
 
 bp = Blueprint("qa", __name__,url_prefix="/")
 # 首页
 # http://127.0.0.1:5000
 @bp.route("/")
 def index():
-   return "qashoye "
+   return "这是首页 "
 
+
+@bp.route("/qa/public", methods=['GET', 'POST'])
+def public_question():
+    if request.method == 'GET':
+        return render_template("public_question.html")
+    else:
+        form = QuestionForm(request.form)
+        if form.validate():
+            title = form.title.data
+            content = form.content.data
+            question = QuestionModel(title=title, content=content, author=g.user)
+            db.session.add(question)
+            db.session.commit()
+            # todo: 跳转到这篇问答的详情页
+            return redirect("/")
+        else:
+            print(form.errors)
+            return redirect(url_for("qa.public_question"))

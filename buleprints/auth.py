27d132d12e91adcb99576b95a05ae7799d334fbc
ixfcao <2717176337@qq.com>
2024-11-18 19:43:32 +0800

    feat:注册验证码验证成功，添加到数据库

--- buleprints/auth.py
+++ buleprints/auth.py
@@ -1,13 +1,14 @@
 import string
 import random
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, url_for
 from mako.runtime import capture
+from werkzeug.security import generate_password_hash
 
-
+from buleprints.forms import RegisterForm
 from exts import mail, db
 from flask_mail import Message
 from flask import redirect
-from models import EmailCaptchaModel
+from models import EmailCaptchaModel, UserModel
 
 bp = Blueprint("auth", __name__,url_prefix="/auth")
 
@@ -15,12 +16,28 @@ bp = Blueprint("auth", __name__,url_prefix="/auth")
 def login():
     return "login"
 
-
-@bp.route("/register")
+# 2717176337@qq.com
+@bp.route("/register",methods=["GET","POST"])
 def register():
     # 验证用户提交的邮箱和验证码是否对应正确
     # 表单验证：flask-wtf：wtforms
-    return render_template("register.html")
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        # 验证用户提交的邮箱和验证码是否对应且正确
+        # 表单验证：flask-wtf: wtforms
+        form = RegisterForm(request.form)
+        if form.validate():
+            email = form.email.data
+            username = form.username.data
+            password = form.password.data
+            user = UserModel(email=email, username=username, password=generate_password_hash(password,method='pbkdf2:sha256'))
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for("auth.login"))
+        else:
+            print(form.errors)
+            return redirect(url_for("auth.register"))
 
 # 验证码
 @bp.route("/captcha/email", methods=['GET'])
@@ -32,7 +49,7 @@ def get_email_captcha():
     source = string.digits * 4
     # 采样
     captcha =  random.sample(source, 4)
-    captcha = " ".join(captcha)
+    captcha = "".join(captcha)
     # I/O :Input/Output 可以用任务队列做
     message = Message(subject="知了传课注册验证码", recipients=[email], body=f"测试！您的验证码是：{captcha}")
     mail.send(message)
@@ -46,10 +63,6 @@ def get_email_captcha():
         "message": "",
         "data": None
     })
-
-
-
-
 
 
 

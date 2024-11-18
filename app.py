8d1edb644eb4569415521b3584a729a672c64eae
ixfcao <2717176337@qq.com>
2024-11-18 20:57:36 +0800

    feat：两个钩子函数

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session,g
 
 from exts import db, mail
 from models import UserModel
@@ -21,6 +21,22 @@ app.register_blueprint(qa_bp)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+# before_request/ before_first_request/ after_request
+# hook
+@app.before_request
+def my_before_request():
+    user_id = session.get("user_id")
+    if user_id:
+        user = UserModel.query.get(user_id)
+        setattr(g, "user", user)
+    else:
+        setattr(g, "user", None)
+
+
+@app.context_processor
+def my_context_processor():
+    return {"user": g.user}
 
 if __name__ == '__main__':
     app.run()
